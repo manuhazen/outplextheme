1777f96407b3e2700882237c03a57069a723d4f5
accountsMM <accounts@marketmentors.com>
2021-07-29 07:49:53 -0400

    Update header.php

--- talk2rep/header.php
+++ talk2rep/header.php
@@ -20,7 +20,19 @@
 	
     <link rel="amphtml" href="http://www.outplex.com/amp/">
 	 -->
-	
+
+
+<!-- Google Tracking code - MM.7.29.21 -->
+<!-- Global site tag (gtag.js) - Google Ads: 652455239 -->
+<script async src=https://www.googletagmanager.com/gtag/js?id=AW-652455239></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-652455239');
+</script>
+<!-- /Google Tracking code - MM.7.29.21 -->
+
 	
 <!-- Bing tracking -->
 <script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"26047441"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
